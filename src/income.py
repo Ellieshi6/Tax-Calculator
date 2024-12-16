2fcd4a64358ebb40f8dfedce41e5e6a5b69f65a9
Ellie Shi <z5592617@ad.unsw.edu.au>
2024-12-17 01:45:53 +1100

    Implemented functions to retrieve income data and calculate taxable income in income.py

---
+++ src/income.py
@@ -0,0 +1,22 @@
+# retrieves user's basic income streams
+def get_income():
+  week_salary = float(input("Please enter your estimated weekly income from wages alone: "))
+  week_salary_weeks = int(input("How many weeks per year do you expect to receive this income? : "))
+  month_investment_income = float(input("Please enter your estimated monthly investment income; i.e. from property, shares, art: "))
+  month_investment_month = int(input("For how many months of the year do you expect to receive this investment income? : "))
+  return {
+    "Weekly Salary": week_salary,
+    "Estimated Working Weeks": week_salary_weeks,
+    "Monthly Investment Income": month_investment_income,
+    "Estimated Months of Investment Income": month_investment_month
+  }
+
+# calculates user's taxable income using user_data
+def taxable_income(user_data):
+  week_salary = user_data["Weekly Salary"]
+  week_salary_weeks = user_data["Estimated Working Weeks"]
+  month_investment_income = user_data["Monthly Investment Income"]
+  month_investment_month = user_data["Estimated Months of Investment Income"]
+  total_taxable_income = (week_salary * week_salary_weeks) + (month_investment_income * month_investment_month)
+  return {"Your Annual Total Taxable Income": total_taxable_income}
+
--- src/main.py
+++ src/main.py
@@ -1,1 +1,32 @@
-test
+from income import get_income, taxable_income
+
+# loop that prints out user's input, and only continues if all entered information is correct
+while True: 
+  user_data = get_income()
+  print('\n')
+  for key, value in user_data.items():
+    if isinstance(value, float):  # Add $ only for float values
+      print(f"{key}: ${value:.2f}")
+    else:
+      print(f"{key}: {value}")
+
+  confirmation = input("\nPlease confirm that this is correct (y/n): ")
+
+  while (confirmation != 'y') or (confirmation != 'n'):
+    if confirmation == 'y':
+      print("\nyay! continuing...\n")
+      break
+    elif confirmation == 'n':
+      print("\noopsies let's fix that\n")
+      break
+    else:
+      print("\nInvalid input, please enter 'y' or 'n'.\n")
+
+    confirmation = input("Please confirm that this is correct (y/n): ")
+  if confirmation == 'y':
+    break
+
+
+taxable_income = taxable_income(user_data)
+for key, value in taxable_income.items():
+  print(f"{key}: ${value:.2f}")
